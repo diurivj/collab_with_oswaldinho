a300350b6fbdb45d90523a35b70c523fe0e86cdb
diurivj <diurivj@icloud.com>
2019-02-11 16:28:19 -0600

    awiwi ahi ta

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.send(`
+    <h1> 
+      Hola Oswaldo
+    </h1>
+  `)
 });
 
 module.exports = router;

088b4fe6d18207eca1f12553d3e888a9df48e541
oswaldo  martinez <oswaldomartinez@MacBook-Pro-de-oswaldo.local>
2019-02-11 16:29:57 -0600

    lol

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router  = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {  
   res.render('index');
 });
+
+router.get('/nuevaruta', (req, res)=>{
+  res.send('Ya vamos a beber!!!')
+})
 
 module.exports = router;

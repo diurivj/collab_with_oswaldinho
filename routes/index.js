a8325d58dbc699d35a4993f16236eb51964d134c
diurivj <diurivj@icloud.com>
2019-02-11 16:32:19 -0600

    ahi ta again x2

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.send(`
-    <h1> 
-      Hola Oswaldo
-    </h1>
-  `)
+  res.send(`Que pedo putita`)
 });
 
 module.exports = router;
